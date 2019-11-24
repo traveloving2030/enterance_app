0b9efe87ca5fba0344d49a0ddfbc0d4ac2264dfa
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2019-11-24 10:38:06 +0900

    123

--- fabric-material/chaincode/enterance-app/enterance-chaincode.go
+++ fabric-material/chaincode/enterance-app/enterance-chaincode.go
@@ -14,7 +14,7 @@
 	 "bytes"
 	 "encoding/json"
 	 "fmt"
-	 "strconv"
+	 
  
 	 "github.com/hyperledger/fabric/core/chaincode/shim"
 	 sc "github.com/hyperledger/fabric/protos/peer"
@@ -28,6 +28,8 @@
  type Enterance struct {
 	 Name string `json:"name"`
 	 Timestamp string `json:"timestamp"`
+	 Location string `json:"location"`
+	 State string `json:"state"`
  }
 
  func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
@@ -39,7 +41,7 @@
  
 	 // Retrieve the requested Smart Contract function and arguments
 	 function, args := APIstub.GetFunctionAndParameters()
-	 // Route to the appropriate handler function to interact with the ledger
+	 // RGENETRATEDe to the appropriate handler function to interact with the ledger
 	 if function == "queryEnterance" {
 		 return s.queryEnterance(APIstub, args)
 	 } else if function == "initLedger" {
@@ -50,6 +52,8 @@
 		 return s.queryAllEnterance(APIstub)
 	 } else if function == "UpdateEnterance" {
 		return s.UpdateEnterance(APIstub, args)
+	} else if function == "queryHistory" {
+		return s.queryHistory(APIstub, args)
 	}
  
 	 return shim.Error("Invalid Smart Contract function name.")
@@ -72,15 +76,24 @@
 
  func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Response {
 	 enterance := []Enterance{
-		 Enterance{Name: "JiWon", Timestamp: "1504054225"},
-		 Enterance{Vessel: "YoungChan", Timestamp: "1504057825"},
+		 Enterance{Name: "JiWon", Timestamp: "2019.11.20", Location: "GATE_A", State: "GENERATED"},
+		 Enterance{Name: "YoungChan", Timestamp: "2019.11.20", Location: "GATE_A", State: "GENETRATED"},
+		 Enterance{Name: "person_A", Timestamp: "2019.11.20", Location: "GATE_A", State: "GENETRATED"},
+		 Enterance{Name: "person_B", Timestamp: "2019.11.20", Location: "GATE_B", State: "GENETRATED"},
+		 Enterance{Name: "person_C", Timestamp: "2019.11.20", Location: "GATE_B", State: "GENETRATED"},
+		 Enterance{Name: "person_D", Timestamp: "2019.11.20", Location: "GATE_B", State: "GENETRATED"},
+		 Enterance{Name: "person_E", Timestamp: "2019.11.20", Location: "GATE_B", State: "GENETRATED"},
+		 Enterance{Name: "person_F", Timestamp: "2019.11.20", Location: "GATE_B", State: "GENETRATED"},
+		 Enterance{Name: "person_G", Timestamp: "2019.11.20", Location: "GATE_B", State: "GENETRATED"},
 	 }
- 
+
+	 nfc_key := [...]string{"dccaa283","d5a5a7c3","a924a283","464da283","f14da7c3","853da7c3","cb02a783","0ad8a283","6e7fa6c3"}
+
 	 i := 0
 	 for i < len(enterance) {
 		 fmt.Println("i is ", i)
 		 enteranceAsBytes, _ := json.Marshal(enterance[i])
-		 APIstub.PutState(strconv.Itoa(i+1), enteranceAsBytes)
+		 APIstub.PutState(nfc_key[i], enteranceAsBytes)
 		 fmt.Println("Added", enterance[i])
 		 i = i + 1
 	 }
@@ -91,13 +104,13 @@
 
  func (s *SmartContract) recordBarcode(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
  
-	 if len(args) != 3 {
+	 if len(args) != 5 {
 		 return shim.Error("Incorrect number of arguments. Expecting 4")
 	 }
  
-	 var enterance = Enterance{ Name: args[1], Timestamp: args[2]}
+	 var enterance = Enterance{ Name: args[1], Timestamp: args[2], Location: args[3], State: args[4]}
  
-	 enteranceAsBytes=, _ := json.Marshal(enterance)
+	 enteranceAsBytes, _ := json.Marshal(enterance)
 	 err := APIstub.PutState(args[0], enteranceAsBytes)
 	 if err != nil {
 		 return shim.Error(fmt.Sprintf("Failed to record Enterance: %s", args[0]))
@@ -108,8 +121,8 @@
 
  func (s *SmartContract) queryAllEnterance(APIstub shim.ChaincodeStubInterface) sc.Response {
 
-	startKey := "0"
-	endKey := "999"
+	startKey := ""
+	endKey := ""
 
 	resultsIterator, err := APIstub.GetStateByRange(startKey, endKey)
 	if err != nil {
@@ -151,7 +164,7 @@
  
 func (s *SmartContract) UpdateEnterance(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
-	if len(args) != 2 {
+	if len(args) != 4 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
@@ -164,7 +177,8 @@ func (s *SmartContract) UpdateEnterance(APIstub shim.ChaincodeStubInterface, arg
 	json.Unmarshal(enteranceAsBytes, &enterance)
 
 	enterance.Timestamp = args[1]
-
+	enterance.Location=args[2]
+	enterance.State=args[3]
 	enteranceAsBytes, _ = json.Marshal(enterance)
 	err := APIstub.PutState(args[0], enteranceAsBytes)
 	if err != nil {
@@ -174,6 +188,66 @@ func (s *SmartContract) UpdateEnterance(APIstub shim.ChaincodeStubInterface, arg
 	return shim.Success(nil)
 }
 
+
+func (s *SmartContract) queryHistory(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	barcodeName := args[0]
+	resultsIterator, err := APIstub.GetHistoryForKey(barcodeName)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing historic values for the marble
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		response, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"TxId\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(response.TxId)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Value\":")
+	
+		if response.IsDelete {
+			buffer.WriteString("null")
+		} else {
+			buffer.WriteString(string(response.Value))
+		}
+
+		// buffer.WriteString(", \"Timestamp\":")
+		// buffer.WriteString("\"")
+		// buffer.WriteString(time.Unix(response.Timestamp.Seconds, int64(response.Timestamp.Nanos)).String())
+		// buffer.WriteString("\"")
+
+		// buffer.WriteString(", \"IsDelete\":")
+		// buffer.WriteString("\"")
+		// buffer.WriteString(strconv.FormatBool(response.IsDelete))
+		// buffer.WriteString("\"")
+
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	return shim.Success(buffer.Bytes())
+}
+
+
  func main() {
  
 	 // Create a new Smart Contract
@@ -182,3 +256,4 @@ func (s *SmartContract) UpdateEnterance(APIstub shim.ChaincodeStubInterface, arg
 		 fmt.Printf("Error creating new Smart Contract: %s", err)
 	 }
  }
+
